b7251d8d3f2270f058040cbce09a3cb94891891b
takumi-seta <takumi.seta@flinters.co.jp>
2023-08-05 00:51:05 +0900

    terraformプロバイダー部分だけ

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.region
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.71.0"
+    }
+  }
+
+  required_version = ">= 1.5.2"
+}
